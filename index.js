bd553649154db753e6676162b7da24dcb20da290
Meysam NajafiTavani <meysamnajafi67@yahoo.com>
2019-11-25 15:51:00 -0800

    save file

--- index.js
+++ index.js
@@ -1,78 +1,76 @@
+const util = require("util");
+const puppeteer = require("puppeteer");
 const fs = require("fs");
 const axios = require("axios");
 const inquirer = require("inquirer");
-const util = require("util");
 
 const expotfile = require("./exportFile");
-const puppeteer = require("puppeteer");
+
 const writeFileAsync = util.promisify(fs.writeFile);
 
 function rendergithubURL() {
-    inquirer
-        .prompt({
-            type: "input",
-            message: "Enter your GitHub username:",
-            name: "username",
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Enter your GitHub username",
+        name: "username"
+      }
+    ])
+    .then(function ({ username }) {
+       //const config = { headers: { accept: "application/json" } };
+      let queryUrl = ` https://api.github.com/users/${username}`;
+      return axios.get(queryUrl ).then(userinformation => {
+        let queryurlnew = `https://api.github.com/users/${username}/starred`;
 
-        })
-        .then(function ({ username }) {
-            const config = { headers: { accept: "application/json" } };
-            const queryUrl = `https://api.github.com/users/${username}`;
+      return  axios.get(queryurlnew).then(represtory => {
+          data = {
+            //getting all information from github and add in onject data
+            img: userinformation.data.avatar_url, location: userinformation.data.location,
+            gitProfile: userinformation.data.html_url, userBlog: userinformation.data.blog,
+            userBio: userinformation.data.bio, repoNum: userinformation.data.public_repos,
+            followers: userinformation.data.followers, following: userinformation.data.following,
+            starNum: represtory.data.length, username: username
 
-            return axios.get(queryUrl, config).then(userinformation => {
-                let queryurlnew = `https://api.github.com/users/${username}/starred`;
+          };
+          // call function 
+          expotfile(data);
+          writeHTMLfile(expotfile(data));
+          createPdf(username);
 
-                axios.get(queryurlnew, config).then(represtory => {
-                  const  data = {
-                        //getting all information from github url and add in onject data
-                        img: userinformation.data.avatar_url, location: userinformation.data.location,
-                        gitProfile: userinformation.data.html_url, userBlog: userinformation.data.blog,
-                        userBio: userinformation.data.bio, repoNum: userinformation.data.public_repos,
-                        followers: userinformation.data.followers, following: userinformation.data.following,
-                        starNum: represtory.data.length, username: username
 
-                    };
-                    // call function 
-                    expotfile( data) ;
-                    xe( expotfile( data) );
-
-                    createPdf (username);
-                });
-
-            });
         });
+      });
+    });
 }
-
-//   call back function to create HTML file 
-
-const xe = function expotfile(){
-    writeFileAsync("index.html", expotfile);
-}
-
-
-
 rendergithubURL();
+// creat call back function to create HTML file 
+  const writeHTMLfile =  expotfile => {
+  writeFileAsync("index.html", expotfile);
+
+}
+
 
 async function createPdf(username) {
-    try {
+  
+    console.log("true");
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.emulateMedia('print');
+    await page.goto("file://C:/Users/meysa/Desktop/testhomework/node/index.html");
 
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-        console.log("true");
-
-        await page.goto("file://C:/Users/meysa/first_day_stuff/nodehm/index.html");
-        await page.emulateMediaFeatures('screen');
-        await page.pdf({
-            path: `${username}.pdf`,
-            format: `A4`,
-            displayHeaderFooter: true,
-            printBackground: true,
-            landscape: true
-
-        })
-    }
-    catch (err) {
-        console.log("err");
-
-    }
+    //To generate a pdf  name and format
+     
+    await page.pdf({ path:`GeneratePDF.pdf`,format: `A4` , printBackground: true});
+    await browser.close();
 }
+
+  
+
+
+  
+  
+
+
+
+ 
